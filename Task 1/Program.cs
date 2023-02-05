0fad25ff752b919a5144aea59eeafa82aa1baa18
AnnaEvgrafova <eaa79evgrafova@yandex.ru>
2023-02-05 13:53:31 +0500

    Урок 6

---
+++ Task 1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 1: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GenerateArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; ++i)
+    {
+        System.Console.Write($"ВВедите число {i + 1}: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+int PositiveNum(int[] number)
+{
+    int positive = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i] > 0)
+        {
+            positive++;
+        }
+    }
+return positive;
+}
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int num = InputInt("Сколько чисел хотите ввести ");
+int[] array = GenerateArray(num);
+PrintArray(array);
+int positiveCount = PositiveNum(array);
+System.Console.WriteLine($"Количество чисел больше 0 = {positiveCount}");
+
---
+++ Task 2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+(double, double) IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+
+bool Parallel(double k1, double k2)
+{
+    if (k1 == k2)
+    {
+        System.Console.WriteLine("Прямые параллельны или совпадают");
+        return false;
+    }
+    return true;
+}
+
+int b1 = InputInt("Введите число b1");
+int k1 = InputInt("Введите число k1");
+int b2 = InputInt("Введите число b2");
+int k2 = InputInt("Введите число k2");
+
+if (Parallel(k1, k2))
+{
+    (double x, double y) = IntersectionPoint(b1, b2, k1, k2);
+    System.Console.WriteLine($"Пересечение в точке: ({x} ; {y})");
+}
